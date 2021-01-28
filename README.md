854cfc725458eaf1e21f6b8aea7933aae9d9e41b
alivit <75130773+alivit@users.noreply.github.com>
2021-01-28 14:00:05 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# pedradaofc
+# Baastin
---
+++ src/help.js
@@ -0,0 +1,93 @@
+
+const help = (prefix) => { 
+	return `                 
+ <══════════════════════>
+ ●-------BAST MOD-------●
+ <══════════════════════>
+
+      ●Ᏼot●ᎷᎬΝႮ●         
+CRIADOR: @5569985001678
+┌────────2.0────────┐
+        VERSÃO
+└────────2.0────────┘
+╔══════════════════════
+║     COMANDOS DO BOT
+╠══════════════════════
+║    🔵CRIADOR🔵
+║╭─────────────────────
+║🔵 ${prefix}fig
+║🔵 ${prefix}figurinha
+║CRIA STICKER (FOTO/VIDEO/GIF)
+║🔵 ${prefix}cachorro
+║🔵 ${prefix}auau
+║🔵 ${prefix}dog
+║DOG ALEATÓRIO
+║├─────────────────────
+║🔵 ${prefix}pokemon
+║POKEMON ALEATÓRIO
+║├─────────────────────
+║🔵 ${prefix}tts pt frase
+║AUDIO RESENHA
+║├─────────────────────
+║🔵 ${prefix}info
+║INFORMAÇÕES DO BOT
+║   🔴ADMINISTRATIVO🔴
+║╭─────────────────────
+║🔴 ${prefix}boasvindas [0/1]
+║MENSAGEM DE BOAS VINDAS OFF/ON
+║├─────────────────────
+║🔴 ${prefix}add 55xxx
+║🔴 ${prefix}adicionar 55xxx
+║ADICIONA MEMBRO
+║├─────────────────────
+║🔴 ${prefix}kick @membro
+║🔴 ${prefix}ban @membro
+║🔴 ${prefix}remover @membro
+║REMOVE MEMBRO
+║├─────────────────────
+║🔴 ${prefix}promote @membro
+║PROMOVE MEMBRO
+║├─────────────────────
+║🔴 ${prefix}demote @adm
+║REBAIXA MEMBRO
+║├─────────────────────
+║🔴 ${prefix}grupo abrir/fechar
+║ABRIR/FECHAR GRUPO
+║├─────────────────────
+║🔴 ${prefix}mudarnome nome
+║TROCAR NOME DO GRUPO
+║├─────────────────────
+║🔴 ${prefix}mudardesc seu txt
+║TROCAR DESCRIÇÃO DO GRUPO
+║├─────────────────────
+║🔴 ${prefix}mudarfoto
+║ALTERAR FOTO DO GRUPO 
+║├─────────────────────
+║🔴 ${prefix}chamada
+║MARCAR MEMBROS
+║├─────────────────────
+║⚪ SUPORTE ⚪
+║⚪ ${prefix}reportar
+║CONVOCA ADMINISTRADORES
+║⚪ ${prefix}reportarus @
+║REPORTAR USUÁRIO AO CRIADOR
+╚══════════════════════
+    ╲    ╱    ●BOT DO HOSPÍCIO●
+ ╱▔▔▔▔▔╲       
+  ┃┈▇┈┈▇┈┃      
+╭╮ ┣━━━━━━┫╭╮    
+┃┃ ┃┈┈┈┈┈┈┃┃┃    
+╰╯ ┃┈┈┈┈┈┈┃╰╯
+   ╰┓┏━━┓┏╯
+  ╰╯      ╰╯
+┌─────────xD────────┐
+     BASE ARR157  
+└─────────xD────────┘
+
+`
+}
+exports.help = help
+
+
+
+  
